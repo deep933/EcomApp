b669b61c1b7b36bea897be1096413b853cc24092
deepmal933 <deepmal944@gmail.com>
2020-08-06 13:56:44 -0400

    connected and fixed cart activity with home activity

---
+++ app/src/main/java/com/example/ecomapp/Cart.java
@@ -0,0 +1,45 @@
+package com.example.ecomapp;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart extends AppCompatActivity {
+
+    private List<Book> books =new ArrayList<>();
+    private ImageView backbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        backbtn = findViewById(R.id.backbtn);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        books.add(new Book("3415","https://m.media-amazon.com/images/I/51Ys5yejqML.jpg","Saint X: A Novel",13.56,"Alexis Schaitkin"));
+        books.add(new Book("3415","https://m.media-amazon.com/images/I/61mlrLANctL.jpg","Things in Jars: A Novel",12.34,"Jess Kidd"));
+        books.add(new Book("3415","https://m.media-amazon.com/images/I/51nyHHSxOLL.jpg","Such a Fun Age",14.70,"Kiley Reid"));
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        CartAdapter cartAdapter = new CartAdapter(this, books);
+        listView.setAdapter(cartAdapter);
+
+    }
+
+}
