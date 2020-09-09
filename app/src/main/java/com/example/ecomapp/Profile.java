b1d8fe2e810727ddfe8879a7b613d9c5485900a4
Sai Akshay <erupakasaiakshay@gmail.com>
2020-09-09 18:07:43 -0400

    added shared prefrence to fetch data in profile

--- app/src/main/java/com/example/ecomapp/Profile.java
+++ app/src/main/java/com/example/ecomapp/Profile.java
@@ -2,11 +2,15 @@ package com.example.ecomapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 public class Profile extends AppCompatActivity {
     private TextView user_name,user_email;
@@ -29,5 +33,22 @@ public class Profile extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private User getUser(){
+        SharedPreferences prefs =getSharedPreferences("userpref",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = prefs.getString("user", "");
+        User user = gson.fromJson(json, User.class);
+        return user;
+    }
+
+    private void logoutUser(){
+        SharedPreferences prefs =getSharedPreferences("userpref",MODE_PRIVATE);
+        prefs.edit().clear().apply();
+        Intent in = new Intent(Profile.this, GettingStarted.class);
+        startActivity(in);
+        finish();
+
     }
 }
