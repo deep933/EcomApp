fa78315206aa0a6ae3ed96d98d88e889161e12dc
deepmal933 <deepmal944@gmail.com>
2020-09-09 19:05:50 -0400

    added shared prefrence in cart activity

--- app/src/main/java/com/example/ecomapp/Cart.java
+++ app/src/main/java/com/example/ecomapp/Cart.java
@@ -1,15 +1,21 @@
 package com.example.ecomapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,5 +47,23 @@ public class Cart extends AppCompatActivity {
         listView.setAdapter(cartAdapter);
 
     }
+
+    public void saveArrayList(ArrayList<String> list, String key){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(Cart.this);
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        editor.putString(key, json);
+        editor.apply();
+    }
+
+    public ArrayList<String> getArrayList(String key){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( Cart.this);
+        Gson gson = new Gson();
+        String json = prefs.getString(key, null);
+        Type type = new TypeToken<ArrayList<String>>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+
 
 }
