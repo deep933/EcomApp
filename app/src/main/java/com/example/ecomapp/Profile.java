977d34cb0a566c245d0847ae1afa53f99d80c4f9
HarshvardhanVummidi <haiharshavardhan@gmail.com>
2020-09-09 16:34:56 -0400

    linked profile view to activity

--- app/src/main/java/com/example/ecomapp/Profile.java
+++ app/src/main/java/com/example/ecomapp/Profile.java
@@ -3,12 +3,31 @@ package com.example.ecomapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
-
+    private TextView user_name,user_email;
+    private Button logout;
+    private ImageView backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        user_email = findViewById(R.id.user_email);
+        user_name = findViewById(R.id.user_name);
+        logout = findViewById(R.id.logout);
+
+        backbtn = findViewById(R.id.backbtn);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
